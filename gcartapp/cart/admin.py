d93c4db686253a74ad80c7f18286ed181e8704b2
amitbhalla <amit9815@gmail.com>
2021-08-31 18:54:04 +0530

    Cart and CartItem models created

--- gcartapp/cart/admin.py
+++ gcartapp/cart/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cart, CartItem
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "cart_id",
+        "date_added",
+    )
+    list_display_links = (
+        "id",
+        "cart_id",
+    )
+    list_filter = [
+        "date_added",
+    ]
+    ordering = [
+        "date_added",
+    ]
+    readonly_fields = ("id",)
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "product",
+        "cart",
+        "is_active",
+    )
+    list_display_links = (
+        "id",
+        "product",
+        "cart",
+    )
+    list_filter = [
+        "product",
+        "is_active",
+    ]
+    ordering = [
+        "id",
+    ]
+    readonly_fields = ("id",)
