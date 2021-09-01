280a4ef5b25d1bd2af50218d9f77e5b3f56fd0cb
amitbhalla <amit9815@gmail.com>
2021-09-01 13:31:53 +0530

    Search Functionality Added

--- gcartapp/store/urls.py
+++ gcartapp/store/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import StoreView, ProductView
+from .views import StoreView, ProductView, SearchPage
 
 urlpatterns = [
-    path("", StoreView.as_view(), name="store"),
+    path(
+        "store/search/",
+        SearchPage.as_view(),
+        name="search",
+    ),
     path(
         "<slug:category_slug>/<slug:product_slug>/",
         ProductView.as_view(),
@@ -13,5 +17,10 @@ urlpatterns = [
         "<slug:category_slug>/",
         StoreView.as_view(),
         name="products_by_category",
+    ),
+    path(
+        "",
+        StoreView.as_view(),
+        name="store",
     ),
 ]
--- gcartapp/store/views.py
+++ gcartapp/store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import base
 from django.core.paginator import Paginator
+from django.db.models import Q
+from django.http.response import Http404
 
 from store.models import Product
 from category.models import Category
@@ -48,3 +50,32 @@ class ProductView(base.View):
             "product": product,
         }
         return render(request, "store/product_detail.html", context)
+
+
+class SearchPage(base.View):
+    def get(self, request):
+
+        products = []
+        product_count = 0
+        if "keyword" in request.GET:
+            searched_term = request.GET["keyword"]
+            if searched_term:
+                try:
+                    products = Product.objects.filter(
+                        Q(description__icontains=searched_term)
+                        | Q(product_name__icontains=searched_term),
+                        is_available=True,
+                    ).order_by("-modified_date")
+                    product_count = products.count()
+                except Product.DoesNotExist:
+                    return Http404()
+
+        paginator = Paginator(products, 6)
+        page = request.GET.get("page")
+        paged_products = paginator.get_page(page)
+        context = {
+            "products": paged_products,
+            "product_count": product_count,
+        }
+
+        return render(request, "store/store.html", context)
